832c0acb7d41bbfaced91780c2f98e9f2b9cc9fa
Sam Brown <Sam@Kays-MacBook-Air.local>
2018-11-28 20:38:49 -0600

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "king";
+var cardFour = "queen";
+
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
