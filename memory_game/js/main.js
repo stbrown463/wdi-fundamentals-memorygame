a143ca986a738c9c1ed1701c5422abe970e549fc
Sam Brown <stbrown463@gmail.com>
2018-12-01 10:43:58 -0600

    refined game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,22 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
